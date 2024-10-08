e8b7ecfc7b4bcef92c8e3ccf9fbe22189cb82197
mberti4110 <mberti@mail.valenciacollege.edu>
2024-10-08 12:55:03 -0400

    Updated Main Class to include login screen.

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,55 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     private static PatientList patients = new PatientList();
     private static TestList tests = new TestList();
     private static FileManager fileManager = new FileManager();
+    private static Menu menu = new Menu();
+
+    //user login. to change with database incorporation.
+    private static Map<Integer, User> users = new HashMap<>();
+    private static Scanner scanner = new Scanner(System.in);
+    private static boolean isAuthenticated = false;
+
+    static {
+        // admin users
+        users.put(1, new User(1, "Berti", "Monica", "password123"));
+        users.put(2, new User(2, "Admin", "User", "admin123"));
+    }
 
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        boolean running = true;
-        Scanner scanner = new Scanner(System.in);
+        // login screen
+        while (!isAuthenticated) {
+            if (login()) {
+                isAuthenticated = true;
+                mainMenu();
+            } else {
+                System.out.println("Invalid credentials, please try again.");
+            }
+        }
+        scanner.close();
+    }
 
+    private static boolean login() {
+        System.out.println("Enter User ID:");
+        int id = Integer.parseInt(scanner.nextLine());  // Read user ID
+        System.out.println("Enter Password:");
+        String password = scanner.nextLine();  // Read password
+
+        User user = users.get(id);
+        if (user != null && user.getOperatorPassword().equals(password)) {
+            System.out.println("Login successful. Welcome, " + user.getOperatorFirstName() + "!");
+            return true;
+        }
+        return false;
+    }
+
+    private static void mainMenu() {
+        boolean running = true;
         while (running) {
             menu.displayMenu();
             int choice = menu.selectOption();
